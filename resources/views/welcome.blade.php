225e368ed6a96859182fae8413b7d0de14c174a5
ghostlyit <supermaximal23@gmail.com>
2021-04-19 07:49:10 +0300

    Обновление кэша

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -24,5 +24,5 @@
     <body class="antialiased">
         <div id="root"></div>
     </body>
-    <script src="{{mix('js/app.js')}}?v=3.0" ></script>
+    <script src="{{mix('js/app.js')}}?v=3.3" ></script>
 </html>
