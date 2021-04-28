0cae0ad3b339777ed6d2750f28ab0c9c9a3077aa
ghostlyit <supermaximal23@gmail.com>
2021-04-28 09:19:52 +0300

    Версия js

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -24,5 +24,5 @@
     <body class="antialiased">
         <div id="root"></div>
     </body>
-    <script src="{{mix('js/app.js')}}?v=3.4" ></script>
+    <script src="{{mix('js/app.js')}}?v=3.5" ></script>
 </html>
